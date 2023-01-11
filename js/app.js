732dcf255c1ae9f838db86ef6a51c9e8f4d9c32f
jrmourad <mouradjebouai@gmail.com>
2023-01-12 00:08:29 +0100

    final_version

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
-var app=Vue.createApp({
+let app=Vue.createApp({
     data(){
         return{
-            brand:'mourad',
+            brand:'Mourad Web Site',
             profile:'',
             name:'',
             subject:'',
             message:'',
+            spinner:true,
             api: 'https://script.google.com/macros/s/AKfycby5Xid0qHc_n3CuULsXbl6NjaWlEmcQ4tCmUFuS3ynEDigbkxfZhzaVz3m8LpnuXR7j/exec'
 
         }
     },
     mounted(){
         //life cycle Hooks
+        this.spinner=true;
          fetch(this.api).then(res=>res.json()).then(res=>{
             console.log(res)
             this.profile=res
+            this.spinner=false;
          }).catch(err=>{
-            console.log(err)
+            console.log(err);
+            this.spinner=false;
          })
     },
     methods:{
         submit(){
-            return `mailto:${this.profile.email}?subject=${this.subject}&body=${this.message}`
+            return `mailto:${this.profile.email}?subject=${this.subject}&body=${this.message}`;
         }
     }
 })
